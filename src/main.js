604d9987e805dccaad8e2080a0bf23dd1ac8d0ed
andy <jiaming_mo@qq.com>
2020-04-17 23:16:50 +0800

    添加vuetify插件和mdi图标依赖

--- src/main.js
+++ src/main.js
@@ -1,17 +1,14 @@
 import Vue from 'vue'
+import vuetify from './plugins/vuetify';
 
 import router from './router'
-
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
 
 import App from './App.vue'
 
 //Vue.config.productionTip = false
 
-Vue.use(ElementUI);
-
 new Vue({
   router,
-  render: h => h(App),
+  vuetify,
+  render: h => h(App)
 }).$mount('#app')
--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,13 +1,73 @@
 <template>
   <div>
-    <h1>Hello</h1>
-    <router-view></router-view>
+    <v-navigation-drawer app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            Application
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            subtext
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <h1>admin</h1>
+    </v-app-bar>
+
+    <!-- Sizes your content based upon application components -->
+    <v-content>
+
+      <!-- Provides the application the proper gutter -->
+      <v-container fluid>
+
+        <!-- If using vue-router -->
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+
+    <v-footer app>
+      <v-spacer></v-spacer>
+      <div>&copy; {{ new Date().getFullYear() }}</div>
+    </v-footer>
   </div>
 </template>
 
 <script>
 export default{
   name: 'Layout',
+  data () {
+    return {
+      items: [
+        { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+        { title: 'Photos', icon: 'mdi-image' },
+        { title: 'About', icon: 'mdi-help-box' },
+      ],
+    }
+  },
 }
 </script>
 
