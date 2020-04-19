6fe7cd2c4977692865918693a82011887b51bf60
andy <jiaming_mo@qq.com>
2020-04-20 01:26:28 +0800

    取消vuetify国际化

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -1,13 +1,13 @@
 import '@mdi/font/css/materialdesignicons.css'
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
-import zhHans from 'vuetify/es5/locale/zh-Hans';
+//import zhHans from 'vuetify/es5/locale/zh-Hans';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
-    lang: {
+    /*lang: {
       locales: { zhHans },
       current: 'zh-Hans',
-    },
+    },*/
 });
