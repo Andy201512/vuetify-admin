3dd52d22522f4afea56440ddf7d3aa5e1e4bd562
andy201512 <1743535684@qq.com>
2021-06-04 10:31:33 +0800

    新增vuex依赖

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import vuetify from './plugins/vuetify';
 
 import router from './router'
 
+import store from './store'
+
 import App from './App.vue'
 
 /*生产环境中要注释掉mock.js*/
@@ -12,6 +14,7 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   vuetify,
   render: h => h(App)
 }).$mount('#app')
--- src/views/main/Dashboard.vue
+++ src/views/main/Dashboard.vue
@@ -1,12 +1,31 @@
 <template>
   <div>
     <h1>Dashboard</h1>
+    <p>How many is the count now?</p>
+    <p>{{ count }} !</p>
+    <v-btn @click="increment">click me to plus one</v-btn>
+    <v-btn @click="reduction">click me to minus one</v-btn>
   </div>
 </template>
 
 <script>
 export default{
   name: 'Dashboard',
+  computed: {
+    count() {
+      return this.$store.state.count
+    }
+  },
+  methods: {
+    increment() {
+      this.$store.commit('increment')
+      // console.log(this.$store.state.count)
+    },
+    reduction() {
+      this.$store.commit('reduction')
+      // console.log(this.$store.state.count)
+    }
+  },
 }
 </script>
 
